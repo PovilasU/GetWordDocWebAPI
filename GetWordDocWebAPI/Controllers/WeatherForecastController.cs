35f332ca7e7859b498ca96034c7b560d5cc2baae
PovilasU <povilas.urbonas85@gmail.com>
2024-01-22 22:12:33 +0000

    test memory steam word doc

---
+++ GetWordDocWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+using System.Net.Http;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Metadata;
+using System.Text;
+//using Microsoft.Office.Interop.Word;
+//using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Word;
+
+
+using System;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+
+
+
+//using System.Web.Http;
+
+namespace GetWordDocWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet]
+        [Route("api/documents/wordfoo")]
+        public IActionResult GetWordDocument()
+        {
+
+            var wordApp = new Application();
+            var wordDoc = wordApp.Documents.Add();
+
+            wordDoc.Content.Text = "Hello, World!";
+
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "YourDocument.docx");
+            wordDoc.SaveAs2(tempFilePath);
+            wordDoc.Close();
+
+            wordApp.Quit();
+
+            var memoryStream = new MemoryStream();
+            using (var fileStream = new FileStream(tempFilePath, FileMode.Open))
+            {
+                fileStream.CopyTo(memoryStream);
+            }
+
+            System.IO.File.Delete(tempFilePath);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return File(memoryStream.ToArray(), "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "YourDocument.docx");
+        }
+
+
+
+
+    }
+
+ 
+}
